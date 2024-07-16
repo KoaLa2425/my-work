0104300f829d50da4b375da31f45e0d3ab3826e3
PawaritPoolso <166476486+Supertao147@users.noreply.github.com>
2024-07-16 22:19:01 +0700

    เพื่อหน้าต่างๆ

--- lib/login.dart
+++ lib/login.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginPageState extends State<LoginPage> {
+  final _formkey = GlobalKey<FormState>();
+  final _navigatorController = GlobalKey<NavigatorState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Navigator(
+      key: _navigatorController,
+      onGenerateRoute: (setting) {
+        return MaterialPageRoute(
+            builder: (context) => Scaffold(
+                  body: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Form(
+                        key: _formkey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.network(
+                              'https://cdn-icons-png.flaticon.com/512/5087/5087579.png',
+                              width: 150,
+                              height: 150,
+                            ),
+                            TextFormField(controller: _usernameController,
+                            decoration: const InputDecoration(labelText: "Username"),
+                            validator: (value){
+                              if (value!.isEmpty){
+                                return 'Pleaes Enter your Username';
+                              }
+                              return null;
+                            } ,
+                            ),
+                            TextFormField(controller: _passwordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(labelText: "Password",),
+                            validator: (value){
+                              if (value!.isEmpty){
+                                return 'Pleaes Enter your Password';
+                              }
+                              return null;
+                            } ,
+                            ),
+                            const SizedBox(height: 10),
+                            ElevatedButton(onPressed: () {
+                              if (_formkey.currentState!.validate()){
+
+                              }
+                            }, 
+                            child: const Text('LogIn'),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+
+                ),
+                
+                );
+      },
+    );
   }
 }
